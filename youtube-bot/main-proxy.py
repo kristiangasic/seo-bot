2c323a82630a9df003460f0ee6b5c2b92c0f5b51
Kristian Gasic <kristian@gasic.bio>
2024-11-24 16:23:22 +0100

    Create main-proxy.py

---
+++ youtube-bot/main-proxy.py
@@ -0,0 +1,5 @@
+Hi, this is currently just a placeholder since the new version with rotating proxies is still in progress.
+I think I’ll be done with it by 15.12.2024. Come back soon :)
+
+Best regards,
+Kristian
